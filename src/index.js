394c922ee9eec815843924688cc573dced929c66
AllaVoitiuk <allavoytjuk@ukr.net>
2022-11-15 00:56:33 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-
-const DEBOUNCE_DELAY = 1000;
-
 import { fetchCountries } from "./fetchcountries";
 
+const DEBOUNCE_DELAY = 300;
 let country = "ukraine";
 
 let refs = {
@@ -14,60 +12,56 @@ let refs = {
     div: document.querySelector ("country-info"),
 }
 
-
 const debounceLoad = debounce(onInputChange, DEBOUNCE_DELAY);
-
 refs.input.addEventListener("input", debounceLoad);
 
-
-
 function onInputChange (event) {  
-   // event.preventDefault(); 
-    let inputValue = refs.input.value
-    
+   event.preventDefault(); 
+    let inputValue = refs.input.value;
+   
     inputValue = inputValue.trim();
-    console.log("Країна: " + inputValue);
-
+    clearInfo();
     if(inputValue){
-        fetchCountries(inputValue)
+     fetchCountries(inputValue)
         .then(data => {
-            console.log(data);
             console.log("data.length: " + data.length);
-            //if (data.length > 2 && data.length < 10) {
+         
             if (data.length === 1) {
                 Notiflix.Notify.success(`success`);
-                //const {results: dataArray} = data;
                 const markupDiv = data.map(item => `
-                    <li>
-                        <img src=${item.flags.svg} alt="">
-                        <p>NAME: ${item.name.official}</p>
-                        <p>capital: ${item.capital}</p>
-                        <p>population: ${item.population}</p>
-                        <p>languages: ${getLanguages(item.languages)}</p>
-                    </li>
-                `).join('');
-
-                //console.log(getLanguages(data[0].languages));
-
-                // <img src=${item.img} alt="">
-                //         <p>NAME: ${item.name}</p>
-                
-                document.querySelector('.country-info').innerHTML += markupDiv
-            //})
+                    
+                    <h2> <img src=${item.flags.svg} alt="">
+                         ${item.name.official}</h2>
+                        <p><b>capital:</b> ${item.capital}</p>
+                        <p><b>population:</b> ${item.population}</p>
+                        <p><b>languages:</b> ${getLanguages(item.languages)}</p>
+                  
+                `).join(''); 
+                           
+                document.querySelector('.country-info').innerHTML = markupDiv
             }
+
+            if (data.length >= 2 && data.length < 10) {
+       
+                const markupUl = data.map(item => `
+                <li>
+                 <img src=${item.flags.svg} alt="">
+                     ${item.name.official}  
+                </li>              
+            `).join(''); 
+            
+            document.querySelector('.country-list').innerHTML = markupUl;
+            }
+            
             if (data.length > 10) {
-                console.log("Too many matches found. Please enter a more specific name.");
+               
                 Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);   
             }
 
-            if (data.length === 0){
+            if (data.length === undefined){
                 Notiflix.Notify.failure(`Oops, there is no country with that name`);
             }
-        });
-        /*.catch((data.length === 0) => {
-            
-            Notiflix.Notify.failure(`Oops, there is no country with that name`);
-          }); */
+        })         
     }
 }
 
@@ -76,8 +70,7 @@ function getLanguages(languages){
     let allLanguages = "";
     let i=0;
 
-    for (let lng in languages)
-    {   
+    for (let lng in languages){   
         if(i > 0){
             allLanguages += ", ";
         }   
@@ -87,34 +80,8 @@ function getLanguages(languages){
     return allLanguages;
 }
 
+function clearInfo() {
+    document.querySelector('.country-list').innerHTML = "";   
+    document.querySelector('.country-info').innerHTML = "";
+}
 
-/*function getData(){
-        fetch('https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages')
-            .then(response => response.json())
-            .then(data => {
-                const {results: dataArray} = data
-                const markup = dataArray.map(item => `
-        <li>
-            <img src=${item.img} alt="">
-            <p>NAME: ${item.name}</p>
-            <p>capital: ${item.capital}</p>
-            <p>population: ${item.population}</p>
-            <p>languages: ${item.languages}</p>
-           
-        </li>
-    `).join('')
-                document.querySelector('.country-list').innerHTML += markup
-            })
-    }
-    getData()*/
-
-//
-// 
-{/* <li>
-<img src=${item.img} alt="">
-<p>NAME: ${item.name}</p>
-<p>capital: ${item.capital}</p>
-<p>population: ${item.population}</p>
-<p>languages: ${item.languages}</p>
-
-</li> */}
